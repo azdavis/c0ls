1105a772ebafff097fac7e9eb649e246184f2c4f
Ariel Davis <ariel.z.davis@icloud.com>
2021-01-27 20:53:56 -0800

    Unwrap less, give exit code

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,28 +1,43 @@
 #![deny(rust_2018_idioms)]
 
 use statics::util::{Cx, ItemDb};
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
 use syntax::ast::{Cast as _, Root};
 
 fn main() {
-  let file = env::args().nth(1).unwrap();
-  let contents = fs::read_to_string(&file).unwrap();
+  match run() {
+    Ok(true) => {}
+    Ok(false) => process::exit(1),
+    Err(e) => {
+      eprintln!("{}", e);
+      process::exit(2);
+    }
+  }
+}
+
+fn run() -> Result<bool, Box<dyn Error>> {
+  let file = match env::args().nth(1) {
+    Some(x) => x,
+    None => return Err("missing first argument".into()),
+  };
+  let contents = fs::read_to_string(&file)?;
   let lex = lex::get(&contents);
   eprintln!("==> lex errors ({})", lex.errors.len());
-  for e in lex.errors {
+  for e in lex.errors.iter() {
     eprintln!("{:?}", e);
   }
   let parse = parse::get(lex.tokens);
   eprintln!("==> parse errors ({})", parse.errors.len());
-  for e in parse.errors {
+  for e in parse.errors.iter() {
     eprintln!("{:?}", e);
   }
   let mut cx = Cx::default();
   let mut items = ItemDb::default();
-  let root = Root::cast(parse.tree).unwrap();
+  let root = Root::cast(parse.tree).expect("parse didn't give a root");
   statics::get(&mut cx, &mut items, root);
   eprintln!("==> statics errors ({})", cx.errors.len());
-  for e in cx.errors {
+  for e in cx.errors.iter() {
     eprintln!("{:?}", e);
   }
+  Ok(lex.errors.is_empty() && parse.errors.is_empty() && cx.errors.is_empty())
 }
