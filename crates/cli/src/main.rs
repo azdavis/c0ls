6fa983ca1af55efbeeb6319f704d7192cb8feae2
Ariel Davis <ariel.z.davis@icloud.com>
2021-01-27 21:05:14 -0800

    Use show_errors macro

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -6,13 +6,24 @@ use syntax::ast::{Cast as _, Root};
 
 fn main() {
   match run() {
-    Ok(true) => {}
+    Ok(true) => eprintln!("no errors"),
     Ok(false) => process::exit(1),
     Err(e) => {
       eprintln!("{}", e);
       process::exit(2);
     }
   }
+}
+
+macro_rules! show_errors {
+  ($name:expr, $errors:expr) => {
+    if !$errors.is_empty() {
+      eprintln!("==> {} errors ({})", $name, $errors.len());
+    }
+    for e in $errors.iter() {
+      eprintln!("{:?}", e);
+    }
+  };
 }
 
 fn run() -> Result<bool, Box<dyn Error>> {
@@ -22,22 +33,13 @@ fn run() -> Result<bool, Box<dyn Error>> {
   };
   let contents = fs::read_to_string(&file)?;
   let lex = lex::get(&contents);
-  eprintln!("==> lex errors ({})", lex.errors.len());
-  for e in lex.errors.iter() {
-    eprintln!("{:?}", e);
-  }
+  show_errors!("lex", lex.errors);
   let parse = parse::get(lex.tokens);
-  eprintln!("==> parse errors ({})", parse.errors.len());
-  for e in parse.errors.iter() {
-    eprintln!("{:?}", e);
-  }
+  show_errors!("parse", parse.errors);
   let mut cx = Cx::default();
   let mut items = ItemDb::default();
   let root = Root::cast(parse.tree).expect("parse didn't give a root");
   statics::get(&mut cx, &mut items, root);
-  eprintln!("==> statics errors ({})", cx.errors.len());
-  for e in cx.errors.iter() {
-    eprintln!("{:?}", e);
-  }
+  show_errors!("statics", cx.errors);
   Ok(lex.errors.is_empty() && parse.errors.is_empty() && cx.errors.is_empty())
 }
