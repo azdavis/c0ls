274fcdb40d4e037a8fcb76f8909e605671d239a4
Ariel Davis <ariel.z.davis@icloud.com>
2021-02-11 18:17:04 -0800

    Add ck-test-data

---
+++ scripts/ck-test-data.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -eu
+
+if [ "$#" -ne 0 ]; then
+  echo "usage: $0"
+  exit 1
+fi
+
+cd "$(dirname "$0")"
+cd ../crates/tests/src
+
+ok=true
+for f in data/*.c0; do
+  if ! git grep -q "$f" -- lib.rs; then
+    echo "$f"
+    ok=false
+  fi
+done
+if ! "$ok"; then
+  echo "error: missing a test for the above files"
+  exit 1
+fi
