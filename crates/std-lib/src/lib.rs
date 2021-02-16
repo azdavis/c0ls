c9ab113346542f119a6f0bfa9374a7550dc6d8e1
Ariel Davis <ariel.z.davis@icloud.com>
2021-02-15 22:34:51 -0800

    Move headers -> data

--- crates/std-lib/src/lib.rs
+++ crates/std-lib/src/lib.rs
@@ -22,14 +22,14 @@ pub struct StdLib {
 pub fn get() -> (Cx, StdLib) {
   let mut cx = Cx::default();
   let std_lib = StdLib {
-    args: get_one(&mut cx, include_str!("headers/args.h0")),
-    conio: get_one(&mut cx, include_str!("headers/conio.h0")),
-    file: get_one(&mut cx, include_str!("headers/file.h0")),
-    img: get_one(&mut cx, include_str!("headers/img.h0")),
-    parse: get_one(&mut cx, include_str!("headers/parse.h0")),
-    rand: get_one(&mut cx, include_str!("headers/rand.h0")),
-    string: get_one(&mut cx, include_str!("headers/string.h0")),
-    util: get_one(&mut cx, include_str!("headers/util.h0")),
+    args: get_one(&mut cx, include_str!("data/args.h0")),
+    conio: get_one(&mut cx, include_str!("data/conio.h0")),
+    file: get_one(&mut cx, include_str!("data/file.h0")),
+    img: get_one(&mut cx, include_str!("data/img.h0")),
+    parse: get_one(&mut cx, include_str!("data/parse.h0")),
+    rand: get_one(&mut cx, include_str!("data/rand.h0")),
+    string: get_one(&mut cx, include_str!("data/string.h0")),
+    util: get_one(&mut cx, include_str!("data/util.h0")),
   };
   (cx, std_lib)
 }
