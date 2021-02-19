a3fd044e65d7c611ea3d4640230fafda6a3ab6a1
Ariel Davis <ariel.z.davis@icloud.com>
2021-02-18 18:23:27 -0800

    Abstract out get_item_loc

--- crates/analysis/src/db.rs
+++ crates/analysis/src/db.rs
@@ -203,29 +203,17 @@ impl Db {
       let expr = syntax_data.ptrs.expr[&AstPtr::new(&expr)];
       match syntax_data.hir_root.arenas.expr[expr] {
         hir::Expr::Call(ref name, _) => {
-          let def_uri_id = semantic_data
-            .import
-            .fns
-            .get(name)
-            .and_then(|x| x.file().uri())
-            .or_else(|| semantic_data.env.fns.contains_key(name).then(|| id))?;
-          let def_syntax_data = &self.syntax_data[&def_uri_id];
-          let item_id =
-            *def_syntax_data.hir_root.items.iter().rev().find(|&&id| {
-              match def_syntax_data.hir_root.arenas.item[id] {
-                hir::Item::Fn(ref the_name, _, _, _) => the_name == name,
-                _ => false,
-              }
-            })?;
-          return Some(Location {
-            uri: self.uris.get(def_uri_id).clone(),
-            range: def_syntax_data.lines.range(
-              def_syntax_data.ptrs.item_back[item_id]
-                .to_node(def_syntax_data.ast_root.syntax().clone())
-                .syntax()
-                .text_range(),
-            ),
-          });
+          return get_item_loc(
+            self,
+            &semantic_data.import.fns,
+            &semantic_data.env.fns,
+            id,
+            name,
+            |item| match *item {
+              hir::Item::Fn(ref the_name, _, _, _) => the_name == name,
+              _ => false,
+            },
+          );
         }
         hir::Expr::Dot(expr, _) => {
           let name = match done.cx.tys.get(semantic_data.env.expr_tys[expr]) {
@@ -245,31 +233,17 @@ impl Db {
           return get_struct_loc(self, semantic_data, id, name)
         }
         hir::Ty::Name(ref name) => {
-          let def_uri_id = semantic_data
-            .import
-            .type_defs
-            .get(name)
-            .and_then(|x| x.file().uri())
-            .or_else(|| {
-              semantic_data.env.type_defs.contains_key(name).then(|| id)
-            })?;
-          let def_syntax_data = &self.syntax_data[&def_uri_id];
-          let item_id =
-            *def_syntax_data.hir_root.items.iter().rev().find(|&&id| {
-              match def_syntax_data.hir_root.arenas.item[id] {
-                hir::Item::TypeDef(ref the_name, _) => the_name == name,
-                _ => false,
-              }
-            })?;
-          return Some(Location {
-            uri: self.uris.get(def_uri_id).clone(),
-            range: def_syntax_data.lines.range(
-              def_syntax_data.ptrs.item_back[item_id]
-                .to_node(def_syntax_data.ast_root.syntax().clone())
-                .syntax()
-                .text_range(),
-            ),
-          });
+          return get_item_loc(
+            self,
+            &semantic_data.import.type_defs,
+            &semantic_data.env.type_defs,
+            id,
+            name,
+            |item| match *item {
+              hir::Item::TypeDef(ref the_name, _) => the_name == name,
+              _ => false,
+            },
+          );
         }
         _ => return None,
       }
@@ -429,19 +403,41 @@ fn get_struct_loc(
   id: UriId,
   name: &hir::Name,
 ) -> Option<Location> {
-  let def_uri_id = semantic_data
-    .import
-    .structs
-    .get(name)
-    .and_then(|x| x.file().uri())
-    .or_else(|| semantic_data.env.structs.contains_key(name).then(|| id))?;
-  let def_syntax_data = &db.syntax_data[&def_uri_id];
-  let item_id = *def_syntax_data.hir_root.items.iter().rev().find(|&&id| {
-    match def_syntax_data.hir_root.arenas.item[id] {
+  get_item_loc(
+    db,
+    &semantic_data.import.structs,
+    &semantic_data.env.structs,
+    id,
+    name,
+    |item| match *item {
       hir::Item::Struct(ref the_name, _) => the_name == name,
       _ => false,
-    }
-  })?;
+    },
+  )
+}
+
+fn get_item_loc<IT, ET, F>(
+  db: &Db,
+  import_items: &FxHashMap<hir::Name, InFile<IT>>,
+  env_items: &FxHashMap<hir::Name, ET>,
+  id: UriId,
+  name: &hir::Name,
+  f: F,
+) -> Option<Location>
+where
+  F: Fn(&hir::Item) -> bool,
+{
+  let def_uri_id = import_items
+    .get(name)
+    .and_then(|x| x.file().uri())
+    .or_else(|| env_items.contains_key(name).then(|| id))?;
+  let def_syntax_data = &db.syntax_data[&def_uri_id];
+  let item_id = *def_syntax_data
+    .hir_root
+    .items
+    .iter()
+    .rev()
+    .find(|&&id| f(&def_syntax_data.hir_root.arenas.item[id]))?;
   Some(Location {
     uri: db.uris.get(def_uri_id).clone(),
     range: def_syntax_data.lines.range(
