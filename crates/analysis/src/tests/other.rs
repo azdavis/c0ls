a37d71067005d67a00be351757ee55aeef7f5a42
Ariel Davis <ariel.z.davis@icloud.com>
2021-02-22 22:58:01 -0800

    Add back an actual header

--- crates/analysis/src/tests/other.rs
+++ crates/analysis/src/tests/other.rs
@@ -5,22 +5,30 @@ use rustc_hash::FxHashMap;
 
 #[test]
 fn change() {
+  let header = r#"
+    int foo();
+  "#;
   let source = r#"
     #use "h.h0"
+
+    int main() {
+      return foo();
+    }
   "#;
   let mut db = Db::new(vec![
-    (uri("/h.h0"), "".to_owned()),
+    (uri("/h.h0"), header.to_owned()),
     (uri("/c.c0"), source.to_owned()),
   ]);
   assert!(db.all_diagnostics().iter().all(|(_, ds)| ds.is_empty()));
   db.update_files(vec![
-    Update::Create(uri("/new.h0"), "".to_owned()),
+    Update::Create(uri("/new.h0"), header.to_owned()),
     Update::Delete(uri("/h.h0")),
   ]);
   let ds: FxHashMap<_, _> = db.all_diagnostics().into_iter().collect();
   assert_eq!(ds.len(), 2);
   assert!(ds[&uri("/new.h0")].is_empty());
   let c_ds = ds.get(&uri("/c.c0")).unwrap();
-  assert_eq!(c_ds.len(), 1);
-  assert_eq!(c_ds.first().unwrap().message, "no such path");
+  assert_eq!(c_ds.len(), 2);
+  assert_eq!(c_ds[0].message, "no such path");
+  assert_eq!(c_ds[1].message, "undefined function `foo`");
 }
