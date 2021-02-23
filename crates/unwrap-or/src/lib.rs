bd9bd4039007d760274d812b16e873bb4564698a
Ariel Davis <ariel.z.davis@icloud.com>
2021-02-22 18:07:20 -0800

    Improve unwrap_or docs

--- crates/unwrap-or/src/lib.rs
+++ crates/unwrap-or/src/lib.rs
@@ -9,16 +9,16 @@
 /// Given `opt` and `or`, this evaluates to `x` if `opt` is `Some(x)`, and `or`
 /// if `opt` is `None`.
 ///
-/// This is just the macro form of the `unwrap_or` method. But because it's a
+/// This is just the macro form of [`Option::unwrap_or`]. But because it's a
 /// macro, the expression `or` can do things with the control flow of the
 /// surrounding expression, like `return` or `break`.
 /// ```
 /// use unwrap_or::unwrap_or;
 ///
-/// let xs = vec![Some(3), None, Some(5)];
+/// let opts = vec![Some(3), None, Some(5)];
 /// let mut sum = 0;
-/// for x in xs {
-///   sum += unwrap_or!(x, continue);
+/// for opt in opts {
+///   sum += unwrap_or!(opt, continue);
 /// }
 /// assert_eq!(sum, 8);
 /// ```
